b43052bd649492f1ebed6a8549d9b6cc9ec4d553
Laurent5 <contact@laurentcardon.be>
2014-03-28 21:55:05 +0100

    Fin de l'initialisation

---
+++ includes/db_connect.php
@@ -0,0 +1,11 @@
+<?php   
+try
+{
+	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+	$bdd = new PDO('mysql:host=localhost;dbname=ropi', 'root', '6wf2buzt', $pdo_options);
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
