93655c77d582c78ca126cd86b9fc35d7ede4206c
Laurent Cardon <Laurent5@users.noreply.github.com>
2014-12-27 18:32:04 +0100

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,7 +2,7 @@
 try
 {
 	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-	$bdd = new PDO('mysql:host=localhost;dbname=ropi', 'root', '6wf2buzt', $pdo_options);
+	$bdd = new PDO('mysql:host=localhost;dbname=ropi', 'root', 'password', $pdo_options);
 	
 }
 catch (Exception $e)
